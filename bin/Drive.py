314001453b726414a674cfd88039d1dca342068d
linshuboy <linshuboy@qq.com>
2017-12-25 22:05:28 +0800

    add Drive class

---
+++ bin/Drive.py
@@ -0,0 +1,67 @@
+import os
+import platform
+import multiprocessing
+
+
+class Drive(object):
+
+    def __init__(self):
+        self.system_name = platform.system()
+        self.cpu_counts = multiprocessing.cpu_count()
+        self.system_size = platform.machine().find('64') > 0 and 64 or 32
+        if self.system_name == 'Linux':
+            self.system_branch_name = self.get_linux_branch_name
+            self.version = self.get_linux_version
+            self.big_version = self.version.split('.')[0]
+            self.is_wsl = self.check_is_wsl
+        elif self.system_name == 'Windows':
+            self.version = platform.version()
+            self.big_version = self.version.split('.')[0]
+            self.system_branch_name = platform.system()+self.big_version
+
+    @property
+    def get_linux_branch_name(self):
+        """
+        返回当前系统的linux分支名称ubuntu/centos/debain
+        :return:
+        """
+        name = 'other'
+        if len(os.popen('which lsb_release').read()) > 0:
+            name = os.popen('lsb_release -is').read().strip('\n')
+        elif self.install_lsb == 0:
+            name = os.popen('lsb_release -is').read().strip('\n')
+        return name
+
+    @property
+    def get_linux_version(self):
+        """
+        返回当前系统的linux分支版本
+        :return:
+        """
+        version = 0
+        if len(os.popen('which lsb_release').read()) > 0:
+            version = os.popen('lsb_release -sr').read().strip('\n')
+        elif self.install_lsb == 0:
+            version = os.popen('lsb_release -sr').read().strip('\n')
+        return version
+
+    @property
+    def check_is_wsl(self):
+        """
+        返回当前系统是否是WSL系统
+        :return:
+        """
+        system = os.popen("uname -r | awk -F- '{print $3}'").read()
+        if system == 'Microsoft':
+            return True
+        else:
+            return False
+
+    @property
+    def install_lsb(self):
+        if len(os.popen('which yum').read()) > 0:
+            order = 'yum'
+            return os.system(order+' install -y redhat-lsb')
+        elif len(os.popen('which apt-get').read()) > 0:
+            order = 'apt-get'
+            return os.system(order+' install -y lsb-release')
